705a67ab060357a6d9f7ce1a57ab9e8624f86d9a
Peter Jakobs <pjakobs@mobileme.de>
2022-09-09 14:01:44 +0200

    initial commit

---
+++ JuncTek_BatteryMonitor.cpp
@@ -0,0 +1,247 @@
+  
+void BatteryMonitor(uint8_t address, Stream &serialDevice){
+  bm_serial=serialDevice;
+  bm_address=address;
+  setValues.deviceAddress=bm_address;
+  basicInfo.deviceAddress=bm_address;
+  measuredValues.deviceAddress=bm_address;
+  
+}
+void ~BatteryMonitor(){
+  
+}
+void setAddress(uint8_t newAddress){
+    
+}
+void setOutoupt(bool output){
+    
+  }
+void setOverVoltageProtection(int voltage){
+  
+}
+void setUnderVoltageProtection(int voltage){
+  
+}
+void setOverCurrentProtection(int current){
+  
+}
+void setUnderCurrentProtection(int current){
+  
+}
+void setOverPowerProtection(int power){
+  
+}
+void setUnderPowerProtection(int power){
+  
+}
+void setBatteryCapacity(int capacity){
+  
+}
+void setVoltageCalibration(int calibrationVoltage){
+  
+}
+void setCurrentCalibration(int calibrationCurrent){
+  
+}
+void setTemperatureCalibration(int calibrationTemperature){
+  
+}
+void setRelayType(int relayType){
+  
+}
+void resetFactorySettings(){
+  
+}
+void setCurrentMultiplier(int currentMultiplier){
+  
+}
+void setBatteryPercent(uint8_t batteryPercent){
+  
+}
+void zeroCurrent(){
+  
+}
+void clearAccountingData(){
+
+}
+
+int getUptime(){
+  
+}
+int getBatteryLifeLeft(){
+  
+}
+int getTemperature(){
+  
+}
+   
+int getProtectionTemperature(){
+  
+}
+int getProtectionRecoveryTime(){
+  
+}
+int getProtectionDelayTime(){
+  
+}
+int getCapacity(){
+  
+}
+int getVoltageCalibration(){
+  
+}
+int getCurrentCalibration(){
+  
+}
+int getTemperatureCalibration(){
+  
+}
+int getVoltageScale(){
+  
+}
+int getCurrentScale(){
+  
+}
+
+float getVoltage() {
+  
+}
+float getCurrent(){
+  
+}
+float getInternalResistance(){
+  
+}
+float getRemainingCapacity(){
+  
+}
+float getCumulativeCapacity(){
+  
+}
+
+float getOVPVoltage(){
+  
+}
+float getUVPVoltage(){
+  
+}
+float getOCPForwardCurrent(){
+  
+}
+float getOCPReverseCurrent(){
+  
+}
+float getOPPPower(){
+  
+}
+
+
+void getBasicInfo(){
+  // result syntax: :r00=<addr>,<checksum>,<sensor_type:1><voltage:1><amperage:2>,<firmware_version>,<machine_serial_nr>,
+  String message;
+  sendMessage(bm_address, BM_F_ReadBasicInf, 1);
+  message=readMessage();
+
+  basicInfo.maxVoltage=getStringField(message,3).toFloat()/100;
+  basicInfo.maxCurrent=getStringField(message,4).toFloat()/100;
+  basicInfo.sensortype=getStringField(message,5).toInt();
+  basicInfo.deviceVersion=getStringField(message,6).toInt();
+  basicInfo.deviceSerialNumber=getStringField(message,7).toInt();
+}
+void getMeasuredValues(){
+  String messsage;
+  sendMessage(bm_addrss, BM_F_ReadMsrdVals, 1);
+  message=readMessage();
+
+  /*
+  * :r50=<addr>,
+  * 01 - <addr>
+  * 02 - <checksum>,
+  * 03 - <Voltage (1/100V)>,
+  * 04 - <Current (1/100A)>,
+  * 05 - <remaining capacity mAh>,
+  * 06 - <cumulative capacity mAh>,
+  * 07 - <Capacity (mWh)>,
+  * 08 - <running time (s)>,
+  * 09 - <temperature (°C+100)>,
+  * 10 - <reserved>,
+  * 11 - <output State>,
+  * 12 - <current direction>,
+  * 13 - <remaining battery life (minutes)>,
+  * 14 - <internal resistance (mOhm/100)>
+  */
+ 
+  measuredValues.uptime=getStringField(message,8).toInt();
+  measuredValues.batteryLifeLeft=getStringField(message,13).toInt();
+  measuredValues.temperature=getStringField(message,9).toInt()-100;
+
+  measuredValues.voltage=getStringField(message,3).toFloat()/100;
+  measuredValues.current=getStringField(message,4).toFloat()/100;
+  measuredValues.internalResistance=getStringField(message,14).toFloat()/100;
+  measuredValues.remainingCapacity=getStringField(message,5).toFloat();
+  measuredValues.cumulativeCapacity=getStringField(message,5).toFloat();
+
+  measuredValues.outputState=getStringField(message,11).toInt();
+  measuredValues.currentDir=getStringField(message,12).toInt();
+}
+void getSetValues(){
+  
+}
+
+String getStringField(String message, int idx){
+  String field;
+  int curr_idx,next_idx;
+  next_idx=message.indexOf("=");
+  //Serial.println(message);
+  for(int i=1;i<=idx;i++){
+    if(next_idx!=-1){
+      curr_idx=next_idx;
+      next_idx=message.indexOf(",",curr_idx+1);
+      Serial.printf("Fieldnum: %i, current idx: %i, next idx: %i\n", i, curr_idx, next_idx);
+    }else{
+      curr_idx=0;         // setting the indexes to 0 and 1
+      next_idx=1;         // will make sure the return string is length 0
+      i=idx;              // exit the loop
+    }
+  }
+  field=message.substring(curr_idx+1,next_idx);
+  return field;
+}
+
+void sendMessage(int address, int command, int parameter){
+  char message[40];
+  sprintf(message, "R%02i:=%i,%i,%i\r\n", command, address, checksum(parameter), parameter);
+  bm_serial.print(message);
+}
+
+String readMessage(){   
+    String _message;
+    msgState_t _msgState;
+    char c;
+    //Serial.println(".....start read loop");
+    if (_msgState==crlf){
+      //Serial.println(".....new message");
+      _message="";
+      _msgState=reading;
+    }
+    
+a    if (sizeof(_message)==0) Serial.print("[start]");
+    while(battMon.available() && _msgState != crlf){
+        c=battMon.read();
+        //Serial.print(c);
+        _message=_message+String(c);
+        //Serial.print(c);
+        if (c=='\r') {
+          _msgState=cr;
+          //Serial.print("{cr}");
+        }
+        if (_msgState==cr && c=='\n') _msgState=crlf;
+        
+        //if (_msgState==reading) Serial.print("[r]");
+        //if (_msgState==cr) Serial.print("[CR]");
+        //if (_msgState==crlf) Serial.println("[CRLF]");
+    }
+    //Serial.println(".....finished read loop");
+    //Serial.println(_message);
+    return _message;
+}
---
+++ JuncTek_BatteryMonitor.h
@@ -0,0 +1,196 @@
+#ifndef _BATTERYMONITORH_
+#define _BATTERYMONITORH_
+
+#define MAXDEVS 4         // max number of battery monitor devices to be supported
+
+
+/* Format:
+ *  
+ *  SOM           :
+ *  Function Code W/R
+ *  Function Nr   00-99
+ *  Connector     =
+ *  Address       1-99
+ *  Spacer        ,
+ *  Checksum      1-255
+ *  data          depending on function, comma separated
+ *  
+ */
+ 
+#define BM_F_SetAddress   01
+#define BM_F_TurnOnOutput 10
+#define BM_F_SetOVProt    20
+#define BM_F_SetUVProt    21
+#define BM_F_SetOCProt    22
+#define BM_F_SetUCProt    23
+#define BM_F_SetOPProt    24
+#define BM_F_SetUPProt    25
+#define BM_F_SetBattCapa  28
+#define BM_F_SetVoltCalbr 29
+#define BM_F_SetCurrCalbr 30
+#define BM_F_SetTempCalbr 31
+#define BM_F_SetRelayType 34
+#define BM_F_ResumeFctSettings 36
+#define BM_F_SetCurrMult  36
+#define BM_F_SetBattPerc  60
+#define BM_F_ZeroCurrent  61
+#define BM_F_ClearAccData 62
+
+#define BM_F_ReadBasicInf 00
+#define BM_F_ReadMsrdVals 50
+#define BM_F_ReadSetVals  51
+
+#define checksum(int a) ((a%255)+1)
+
+typedef enum {
+        reading,
+        cr,
+        crlf
+}msgState_t;
+
+typedef  struct{
+    int 
+        deviceAddress,
+        checksum,
+        maxVoltage,
+        maxCurrent;
+
+    enum sensorType{
+        HALLSENSOR = 1,
+        SAMPLER=2
+    };
+    
+    int deviceVersion;
+    int deviceSerialNumber;
+} basicInfo_t;
+
+
+typedef struct {
+    int 
+        deviceAddress,
+        checksum,    
+        uptime,
+        batteryLifeLeft,
+        temperature;
+
+    float
+        voltage,
+        current,
+        internalResistance,
+        remainingCapacity,
+        cumulativeCapacity;
+
+    enum outputState{
+        ON = 0,
+        OVP = 1,
+        OCP = 2,
+        LVP = 3,
+        NCP = 4,
+        OPP = 5,
+        OTP = 6,
+        OFF =255
+    };
+
+    enum currentDir{
+        discharging = 0,
+        charging =1
+    };
+}measuredValues_t;
+
+typedef struct{
+    int 
+        deviceAddress,
+        checksum,
+        protectionTemperature,
+        protectionRecoveryTime,
+        protectionDelayTime,
+        presetCapacity,
+        voltageCalibration,
+        currentCalibration,
+        temperatureCalibration,
+        voltageScale,
+        currentScale;
+
+    float        
+        OVPVoltage,
+        UVPVoltage,
+        OCPForwardCurrent,
+        OCPReverseCurrent,
+        OPPPower;
+
+    enum relayType{
+        normallyOpen=0,
+        normallyClosed=1
+    };
+}setValues_t;
+
+class BatteryMonitor{
+  public
+  BatteryMonitor(uint8_t address, Stream &serialDevice);
+  ~BatteryMonitor();
+  void
+    changeAddress(uint8_t newAddress),
+    setOutoupt(bool output),
+    setOverVoltageProtection(int voltage),
+    setUnderVoltageProtection(int voltage),
+    setOverCurrentProtection(int current),
+    setUnderCurrentProtection(int current),
+    setOverPowerProtection(int power),
+    setUnderPowerProtection(int power),
+    setBatteryCapacity(int capacity),
+    setVoltageCalibration(int calibrationVoltage),
+    setCurrentCalibration(int calibrationCurrent),
+    setTemperatureCalibration(int calibrationTemperature),
+    setRelayType(int relayType),
+    resetFactorySettings(),
+    setCurrentMultiplier(int currentMultiplier),
+    setBatteryPercent(uint8_t batteryPercent),
+    zeroCurrent(),
+    clearAccountingData();
+
+  int 
+    getUptime(),
+    getBatteryLifeLeft(),
+    getTemperature(),
+   
+    getProtectionTemperature(),
+    getProtectionRecoveryTime(),
+    getProtectionDelayTime(),
+    getCapacity(),
+    getVoltageCalibration(),
+    getCurrentCalibration(),
+    getTemperatureCalibration(),
+    getVoltageScale(),
+    getCurrentScale();
+
+float
+    getVoltage(),
+    getCurrent(),
+    getInternalResistance(),
+    getRemainingCapacity(),
+    getCumulativeCapacity(),
+
+    getOVPVoltage(),
+    getUVPVoltage(),
+    getOCPForwardCurrent(),
+    getOCPReverseCurrent(),
+    getOPPPower();
+
+  private
+  void
+    getBasicInfo(),
+    getMeasuredValues(),
+    getSetValues();
+  String 
+    readMessage(),
+    getStringField(String message, int idx);
+;
+
+  setValues_t       setValues;
+  measuredValues_t  measuredValues;
+  basicInfo_t       basicInfo;
+  int               bm_address
+  Stream            bm_serial;
+}
+
+#endif
